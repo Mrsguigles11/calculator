ec93cb78e001d62bb203f9995fc7584793993031
Harry Woodgate <harry_woodgate11@hotmail.co.uk>
2024-04-16 16:10:53 +0100

    Create the necessary files. Add boilerplate to HTML and the basic operator functions for the JS.

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, oper, b) {
+    return oper(a, b);
+}
+
+console.log(operate(2, subtract, 4));
